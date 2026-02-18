645769bf63611619522b850cc1f6003bdb027bf3
Aditya Panicker <adipanicker7@gmail.com>
2026-02-18 14:11:27 +0530

    allowed * hosts -- fixed typo

--- ecommerce/ecomm/settings.py
+++ ecommerce/ecomm/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^q6g%wyn970a)6so28vs@l@%wj8=-srn==#j8wgx25rgpmv$e7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
